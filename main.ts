8cf3d0f4c7c0526f5dab97d6a2164dea98c527c9
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-11-08 15:39:46 -0500

    radar

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 Isaac Ip All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Nov 2025
+ * This program uses the bluetooth radios
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(12)
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P0, 
+        DigitalPin.P0, 
+        PingUnit.Centimeters)
+        basic.showNumber(distanceToObject)
+        if (distanceToObject <= 10) {
+            radio.sendString("Too Close!")
+        }
+}
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
