0d8079cb00a24a776382ff3b752922f192a246ae
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-11-08 15:48:03 -0500

    save and finish when sonar is ready

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Isaac Ip
+Created on: Nov 2025
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import radio
 
+# variables
+distanceToObject = 0
 
-display.scroll("Hello, World!")
+radio.on()
+radio.config(channel=7)
+
+while True:
+    
