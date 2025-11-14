72f0499a74dbd3a34850c042983356107df0516c
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-11-14 12:55:56 -0500

    working radio

--- main.ts
+++ main.ts
@@ -9,22 +9,34 @@
 let distanceToObject: number = 0
 
 // setup
-radio.setGroup(12)
+radio.setGroup(247)
 basic.showIcon(IconNames.Happy)
 
-while (true) {
-    distanceToObject = sonar.ping(
-        DigitalPin.P0, 
-        DigitalPin.P1, 
-        PingUnit.Centimeters)
-        basic.showNumber(distanceToObject)
-        if (distanceToObject < 10) {
-            radio.sendString("Too Close!")
-        }
-}
-
+// receive
 radio.onReceivedString(function (receivedString) {
     basic.clearScreen()
     basic.showString(receivedString)
     basic.showIcon(IconNames.Happy)
 })
+
+// loop
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P12,
+        DigitalPin.P13,
+        PingUnit.Centimeters
+    )
+
+    // if distance is less than 5
+    if (distanceToObject < 5) {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("Too close")
+        basic.pause(1000)
+    } else {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("")
+        basic.pause(1000)
+    }
+}
